840608bde607524236a872948a7939db68cdf591
tsakov <etsakov@asteasolutions.com>
2013-07-31 18:11:51 +0300

    Testing some stuff

---
+++ src/quickEditor/infusion/README.txt
@@ -0,0 +1,26 @@
+MyInfusion was built from an unreleased version of Infusion branch https://github.com/jobara/infusion/tree/FLUID-4907, commit c8b5ba356e876f8b3756c3b40b2907bbb88055c3, using the following command:
+
+Build the minified Infusion:
+ant customBuild -Dinclude="enhancement, uiOptions, progress, tooltip, jQueryScrollToPlugin" -lib lib/rhino
+
+Build the un-minified Infusion:
+ant customBuild -Dinclude="enhancement, uiOptions, progress, tooltip, jQueryScrollToPlugin" -lib lib/rhino -DnoMinify="true"
+
+
+
+Customizations of UI Options
+============================
+
+The following edits have been made to the UI Options HTML files:
+
+FatPanelUIOptionsFrame.html:
+32,38c32
+<         <script type="text/javascript" src="../../../lib/jquery/core/js/jquery.js"></script>
+<         <script type="text/javascript" src="../../../lib/jquery/ui/js/jquery.ui.core.js"></script>
+<         <script type="text/javascript" src="../../../lib/jquery/ui/js/jquery.ui.widget.js"></script>
+<         <script type="text/javascript" src="../../../lib/jquery/ui/js/jquery.ui.mouse.js"></script>
+<         <script type="text/javascript" src="../../../lib/jquery/ui/js/jquery.ui.slider.js"></script>
+<         <script type="text/javascript" src="../../../lib/jquery/plugins/touchPunch/js/jquery.ui.touch-punch.js"></script>
+<         <script type="text/javascript" src="../../../lib/json/js/json2.js"></script>
+---
+>          <script type="text/javascript" src="../../../MyInfusion.js"></script>
--- src/quickEditor/quickEditor.js
+++ src/quickEditor/quickEditor.js
@@ -1,17 +1,4 @@
 (function ($, fluid) {
-    fluid.defaults("catOptions.primarySchema", {
-        gradeNames: ["fluid.uiOptions.schema", "autoInit"],
-        schema: {
-            "catOptions.size": {
-                "type": "number",
-                "default": 1,
-                "minimum": 1,
-                "maximum": 10,
-                "divisibleBy": 1
-            }
-        }
-    });
-
     fluid.defaults("catOptions.panel", {
         gradeNames: ["fluid.uiOptions.panels", "autoInit"],
         preferenceMap: {
@@ -23,58 +10,53 @@
         },
          
         range: {
-            min: 1,
-            max: 2
+            min: null,
+            max: null
         },
          
         selectors: {
             catSizeSlider: "#size",
             label: ".catpanel-size-label"
         },
-                 
+
         protoTree: {
-            "catSizeSlider": {
-                    valuebinding: "{value}",
-                    decorators: {
-                        attrs: {
-                            min: "{that}.options.range.min",
-                            max: "{that}.options.range.max"
-                        }
+            catSizeSlider: {
+                valuebinding: "{value}",
+                decorators: {
+                    attrs: {
+                        min: "{that}.options.range.min",
+                        max: "{that}.options.range.max"
                     }
+                }
             },
             label: {
                 messagekey: "catpanel.catsize"
             }
-        },
-         
-        classnameMap: null, // must be supplied by implementors
-        controlValues: {
-            textFont: ["default", "times", "comic", "arial", "verdana"]
         }
     });
 
-    fluid.defaults("catOptions.auxSchema", {
-        gradeNames: ["fluid.uiOptions.auxSchema", "autoInit"],
-        auxiliarySchema: {
-            "namespace": "catOptions",
-            "templatePrefix": "../pages/catOptionsPanel/html/",
-            "catSize": {
-                "type": "catOptions.size"
-            },
-             
-            "enactors": [
-                {
-                    "type": "catOptions.enactors.catSize"
-                }
-            ],
-             
-            "panels": [
-                {
-                    "type": "catOptions.panel",
-                    "container": ".catOptions-catSizePanel",  // the css selector in the template where the panel is rendered
-                    "template": "./catPanel.html"
-                }
-            ]
+    catOptions.primarySchema = {
+        "catOptions.size": {
+            "type": "number",
+            "default": 1,
+            "minimum": 1,
+            "maximum": 10,
+            "divisibleBy": 1
         }
-    });
+    };
+
+    catOptions.auxSchema = {
+        namespace: "catOptions",
+        //templatePrefix: "",
+        //messagePrefix: "",
+        catSize: {
+            type: "catOptions.size",
+            panel: {
+                type: "catOptions.panel",
+                container: ".catOptions-catSizePanel",
+                template: "./catpanel.html",
+                message: "./cat_bg.json"
+            }
+        }
+    };
 })(jQuery, fluid);
